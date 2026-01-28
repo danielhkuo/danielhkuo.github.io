0ac28c70916e4d0bf84f658ac2a55751a9cd14b8
Daniel Kuo <48892016+danielhkuo@users.noreply.github.com>
2026-01-28 11:45:44 -0600

    remove old portfolio stack

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Kuofolio';
-export const SITE_DESCRIPTION = 'Daniel Kuo\'s Personal Site';
